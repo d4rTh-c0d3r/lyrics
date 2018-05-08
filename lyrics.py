f5919d156546eaf0535a4cbac162bfdc519b8181
d4rTh-c0d3r <gurparkashsohi@gmail.com>
2018-05-09 01:46:45 +0530

    Uploading Files

---
+++ lyrics.py
@@ -0,0 +1,20 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+
+def strip(name):
+	name = re.sub(r'^a ','',name)
+	name = re.sub(r'^an ','',name)
+	name = re.sub(r'^the ','',name)
+	return name
+
+def convert(name):
+	name = re.sub(r'[^a-z0-9]', '', name)
+	return name
+
+song = convert(input("Song: ").lower())
+singer = convert(strip(input("Artist: ").lower()))
+
+url = "https://www.azlyrics.com/lyrics/" + singer + "/" + song + ".html"
+html = urllib.request.urlopen(url).read()
+print(BeautifulSoup(html, 'html.parser').find_all('div')[21].text)
